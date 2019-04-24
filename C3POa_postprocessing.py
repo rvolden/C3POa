172fc2e8ec81d35e3a1da22a9a04e31fe1ad3a62
Roger Volden <rvolden@ucsc.edu>
2019-04-24 11:07:26 -0700

    Added an undirectional flags and trim flag

--- C3POa_postprocessing.py
+++ C3POa_postprocessing.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # Roger Volden and Chris Vollmers
-# Last updated: 28 Feb 2019
+# Last updated: 22 May 2018
 
 import sys
 import os
 import argparse
-import numpy as np
 
 def argParser():
     '''Parses arguments.'''
@@ -19,12 +18,26 @@ def argParser():
                         help='If you want to use a config file to specify paths to\
                               programs, specify them here. Use for poa, racon, water,\
                               blat, and minimap2 if they are not in your path.')
+    parser.add_argument('--undirectional','-u', action='store_true',
+                        help='By default, your cDNA molecules are assumed to be \
+                              directional with two sequences named "3Prime_adapter" \
+                              and "5Prime_adapter" expected in your adapter_file in \
+                              fasta format. If you add this flag your cDNA molecules \
+                              are expected to be undirectional and only one sequence \
+                              named "Adapter" should be in your adapter_file in fasta \
+                              format')
+    parser.add_argument('--trim', '-t', action='store_true',
+                        help='Use this flag to trim the adapters off the ends of \
+                              your sequences.')
+
     return vars(parser.parse_args())
 
 args = argParser()
 output_path = args['output_path'] + '/'
 input_file = args['input_fasta_file']
 adapter_file = args['adapter_file']
+undirectional = args['undirectional']
+trim = args['trim']
 
 def configReader(configIn):
     '''Parses the config file.'''
@@ -63,25 +76,16 @@ else:
 def read_fasta(inFile):
     '''Reads in FASTA files, returns a dict of header:sequence'''
     readDict = {}
-    tempSeqs, headers, sequences = [], [], []
     for line in open(inFile):
         line = line.rstrip()
         if not line:
             continue
         if line.startswith('>'):
-            headers.append(line.split()[0][1:])
-        # covers the case where the file ends while reading sequences
-        if line.startswith('>'):
-            sequences.append(''.join(tempSeqs).upper())
-            tempSeqs = []
+            readDict[line[1:]] = ''
+            lastHead = line[1:]
         else:
-            tempSeqs.append(line)
-    sequences.append(''.join(tempSeqs).upper())
-    sequences = sequences[1:]
-    for i in range(len(headers)):
-        repeat = headers[i].split('_')[-2]
-        readDict[headers[i]] = sequences[i]
-    return readDict
+            readDict[lastHead] += line
+    return readDict, len(readDict) * 2
 
 def reverse_complement(sequence):
     '''Returns the reverse complement of a sequence'''
@@ -89,7 +93,7 @@ def reverse_complement(sequence):
     return ''.join([bases[x] for x in list(sequence)])[::-1]
 
 def run_blat(path, infile, adapter_fasta):
-    os.system('%s -noHead -stepSize=1 -tileSize=6 -t=DNA q=DNA -minScore=15 \
+    os.system('%s -noHead -stepSize=1 -tileSize=6 -t=DNA q=DNA -minScore=10 \
               -minIdentity=10 -minMatch=1 -oneOff=1 \
               %s %s %s/Adapter_to_consensus_alignment.psl' \
               %(blat,adapter_fasta,infile,path))
@@ -97,9 +101,11 @@ def run_blat(path, infile, adapter_fasta):
 def parse_blat(path, infile, length):
     adapter_dict, iterator = {}, 0
     infile1 = open(infile, 'r')
+    print(length)
     while iterator < length:
         line = infile1.readline()
         sequence = infile1.readline()
+
         name = line[1:].strip()
         adapter_dict[name] = {}
         adapter_dict[name]['+'] = []
@@ -126,41 +132,60 @@ def parse_blat(path, infile, length):
     return adapter_dict
 
 def write_fasta_file(path, adapter_dict, reads):
-    out = open(path + 'R2C2_full_length_consensus_reads_R2.fasta', 'w')
-
+    out = open(path + 'R2C2_full_length_consensus_reads.fasta', 'w')
+    out3 = open(path + 'R2C2_full_length_consensus_reads_left_splint.fasta', 'w')
+    out5 = open(path + 'R2C2_full_length_consensus_reads_right_splint.fasta', 'w')
     for name, sequence in reads.items():
-        qual = float(name.split('_')[1])
-        if qual >= 9:
-            adapter_plus = sorted(adapter_dict[name]['+'],
-                                  key=lambda x: x[2], reverse=False)
-            adapter_minus = sorted(adapter_dict[name]['-'],
-                                  key=lambda x: x[2], reverse=False)
-            plus_list_name, plus_list_position = [], []
-            minus_list_name, minus_list_position = [], []
-            for adapter in adapter_plus:
-                if adapter[0] != '-':
-                    plus_list_name.append(adapter[0])
-                    plus_list_position.append(adapter[2])
-            for adapter in adapter_minus:
-                if adapter[0] != '-':
-                    minus_list_name.append(adapter[0])
-                    minus_list_position.append(adapter[2])
+        adapter_plus = sorted(adapter_dict[name]['+'],
+                              key=lambda x: x[2], reverse=False)
+        adapter_minus = sorted(adapter_dict[name]['-'],
+                              key=lambda x: x[2], reverse=False)
+        plus_list_name, plus_list_position = [], []
+        minus_list_name, minus_list_position = [], []
+        for adapter in adapter_plus:
+            if adapter[0] != '-':
+                plus_list_name.append(adapter[0])
+                plus_list_position.append(adapter[2])
+        for adapter in adapter_minus:
+            if adapter[0] != '-':
+                minus_list_name.append(adapter[0])
+                minus_list_position.append(adapter[2])
 
-            if len(plus_list_name) == 1 and len(minus_list_name) == 1:
-                if plus_list_position[0] < minus_list_position[0]:
-                    seq = sequence[plus_list_position[0]:minus_list_position[0]]
-                    ada = sequence[plus_list_position[0]-40:minus_list_position[0]+40]
-                    name += '_' + str(len(seq))
-                    if '5Prime_adapter' in plus_list_name[0] \
-                       and '3Prime_adapter' in minus_list_name[0]:
-                        out.write('>%s\n%s\n' %(name, ada))
-                    elif '3Prime_adapter' in plus_list_name[0] \
-                         and '5Prime_adapter' in minus_list_name[0]:
-                        out.write('>%s\n%s\n' %(name, reverse_complement(ada)))
+        if len(plus_list_name) == 1 and len(minus_list_name) == 1:
+            if plus_list_position[0] < minus_list_position[0]:
+                seq = sequence[plus_list_position[0]:minus_list_position[0]]
+                ada = sequence[plus_list_position[0]-40:minus_list_position[0]+40]
+                name += '_' + str(len(seq))
+                use = False
+                if undirectional:
+                    direction = '+'
+                    use = True
+                else:
+                    if plus_list_name[0] != minus_list_name[0]:
+                        use = True
+                        if plus_list_name[0] == '5Prime_adapter':
+                            direction = '+'
+                        elif plus_list_name[0] == '3Prime_adapter':
+                            direction = '-'
+                if use:
+                    if direction == '+':
+                        if trim:
+                            out.write('>%s\n%s\n' %(name, seq))
+                        else:
+                            out.write('>%s\n%s\n' %(name, ada))
+                        out5.write('>%s\n%s\n' %(name, reverse_complement(sequence[:plus_list_position[0]])))
+                        out3.write('>%s\n%s\n' %(name, sequence[minus_list_position[0]:]))
+                    elif direction == '-':
+                        if trim:
+                            out.write('>%s\n%s\n' %(name, reverse_complement(seq)))
+                        else:
+                            out.write('>%s\n%s\n' %(name, reverse_complement(ada)))
+                        out3.write('>%s\n%s\n' %(name, reverse_complement(sequence[:plus_list_position[0]+40])))
+                        out5.write('>%s\n%s\n' %(name, sequence[minus_list_position[0]:]))
 
 def main():
-    reads = read_fasta(input_file)
-    fileLen = len(reads)*2
+    reads, fileLen = read_fasta(input_file)
+
     run_blat(output_path, input_file, adapter_file)
     adapter_dict = parse_blat(output_path, input_file, fileLen)
     write_fasta_file(output_path, adapter_dict, reads)
