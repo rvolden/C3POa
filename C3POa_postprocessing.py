cd641403d28d5ee389fb2b5ff6ff10f98043fc9f
Roger Volden <rvolden@ucsc.edu>
2019-08-27 10:19:00 -0700

    Changed the fasta reader to be able to handle genomes

--- C3POa_postprocessing.py
+++ C3POa_postprocessing.py
@@ -81,10 +81,13 @@ def read_fasta(inFile):
         if not line:
             continue
         if line.startswith('>'):
-            readDict[line[1:]] = ''
+            if readDict:
+                ''.join(readDict[lastHead])
+            readDict[line[1:]] = []
             lastHead = line[1:]
         else:
-            readDict[lastHead] += line
+            readDict[lastHead].append(line)
+    ''.join(readDict[lastHead])
     return readDict
 
 def reverse_complement(sequence):
