72edd6a1d0eaa90b7a099b393b30368b4947429c
Roger Volden <rvolden@ucsc.edu>
2020-10-30 19:02:58 -0700

    Took out debug comments

--- bin/call_peaks.py
+++ bin/call_peaks.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
 # Roger Volden
 
-'''
-Need to rewrite this to suit the new way we're calling peaks with scipy.
-
-This will just be given the score list.
-It'll smooth it over with the Savitzky Golay filter and then call local
-maxima using scipy.signal.
-This should ultimately return a list of peaks.
-'''
-
 from savitzky_golay import savitzky_golay
 from scipy.signal import find_peaks
 import numpy as np
